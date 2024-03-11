476a5f677ef21dd296697534e7b906f81e3c07a9
BenithaSri <156029071+BenithaSri@users.noreply.github.com>
2024-03-11 03:19:47 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var x = Math.floor(Math.random()*100);
-var y = Math.floor(Math.random()*100);
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
+    var x = Math.floor(Math.random() * 50);
+    var y = Math.floor(Math.random() * 50);
+
+
     function calcAngleDegrees(x, y) {
         return (Math.atan2(y, x) * 180) / Math.PI;
     }
-
+    console.log(x);
+    console.log(y);
     console.log(calcAngleDegrees(x, y));
     res.send(`Math.atan2() applied to ${x} and  ${y} is ${calcAngleDegrees(x, y)}`);
 });
